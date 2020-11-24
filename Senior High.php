3f7a7322140b1f87f9273ac16e5fc03a57337119
Fritz1820646 <74946371+Fritz1820646@users.noreply.github.com>
2020-11-24 23:14:49 +0800

    Update Senior High.php

--- Senior High.php
+++ Senior High.php
@@ -13,14 +13,14 @@
 	<div class="header">
 	 <h1>Senrior High School</h1>
 	 <hr style='width:50%;' />
-	 <h2 style='color: #000000;'>Londres, John Hendrics</h2>
+	 <h2 style='color: #000000;'>Demegillo, Fritz Lowell</h2>
 	</div>
 	  
 
 <div class="responsive">
   <div class="gallery">
-    <a href="img_5terre.jpg">
-      <img src="Picture/s1.jpg" alt="Cinque Terre" width="600" height="400">
+    <a href="KDA_akali.jpg">
+      <img src="KDA_akali.jpg" alt="Akali" width="600" height="400">
     </a>
     <div class="desc">Add a description of the image here</div>
   </div>
@@ -29,8 +29,8 @@
 
 <div class="responsive">
   <div class="gallery">
-    <a href="Picture/Koala.jpg">
-      <img src="Picture/s2.jpg" alt="Forest" width="600" height="400">
+    <a href="KDA_kai'sa.jpg">
+      <img src="KDA_kai'sa.jpg" alt="Kai'sa" width="600" height="400">
     </a>
     <div class="desc">Add a description of the image here</div>
   </div>
@@ -38,8 +38,8 @@
 
 <div class="responsive">
   <div class="gallery">
-    <a href="img_lights.jpg">
-      <img src="Picture/s3.jpg" alt="Northern Lights" width="600" height="400">
+    <a href="KDA_evelyn.jpg">
+      <img src="KDA_evelyn.jpg" alt="Evelyn" width="600" height="400">
     </a>
     <div class="desc">Add a description of the image here</div>
   </div>
@@ -47,8 +47,8 @@
 
 <div class="responsive">
   <div class="gallery">
-    <a href="img_mountains.jpg">
-      <img src="Picture/s4.jpg" alt="Mountains" width="600" height="400">
+    <a href="KDA_ahri.jpg">
+      <img src="KDA_ahri.jpg" alt="Ahri" width="600" height="400">
     </a>
     <div class="desc">Add a description of the image here</div>
   </div>
@@ -57,8 +57,8 @@
 <div class="clearfix"></div>
 
 <div style="padding:6px;">
-  <p>Sorry Sir idon have picture in Elementary. thank you!</p>
-  <p>You will learn more about media queries and responsive web design later in our CSS Tutorial.</p>
+
+  <p>Enjoy!!</p>
 </div>
 		
    </body>
